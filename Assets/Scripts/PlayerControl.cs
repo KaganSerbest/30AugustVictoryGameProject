82f953cac487c1feb0fd04e6d57cb9975a2f0c2e
Bengisu Sagir <bengisusagir@gmail.com>
2023-08-23 02:56:49 +0300

    playr

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    public Animator animator;
+    public GameObject cam;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetBool("isWalking", true);
+            transform.Translate(new Vector3(0, 0, 2f) * Time.deltaTime);
+        }
+        else
+            animator.SetBool("isWalking", false);
+
+        if (animator.GetBool("isWalking") && Input.GetKey(KeyCode.LeftControl))
+        {
+            animator.SetBool("isWalkingToR", true);
+            transform.Translate(new Vector3(0, 0, 4f) * Time.deltaTime);
+        }
+        else
+            animator.SetBool("isWalkingToR", false);
+
         
     }
 }
